d5d835f28181ba71eea730c9342aed6354116a4a
Paul-Michael Clarke <paul.clarke@mybcc.edu.bb>
2024-05-13 23:29:44 -0400

    password generator

--- GeneratePassword.js
+++ GeneratePassword.js
@@ -1,7 +1,45 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View,Text, } from 'react-native';
+import { Button, TextInput } from 'react-native-paper';
 
 export default function GeneratePassword() {
+    const[password, setPassword] = useState("");
+
+    function createPassword(){
+        const length = 8;
+        const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const lowercase = "abcdefghijklmnopqrstuvwxyz";
+        const number = "1234567890";
+        const symbol = "~!@#$%^&*()_+=-`{}[]:;<>?/\*"
+        const allChars = uppercase + lowercase + number + symbol;
+
+        let password = "";
+
+        password += uppercase[Math.floor(Math.random() * uppercase.length)];
+        password += lowercase[Math.floor(Math.random() * lowercase.length)];
+        password += number[Math.floor(Math.random() * number.length)];
+        password += symbol[Math.floor(Math.random() * symbol.length)];
+
+        while(length > password.length){
+            password += allChars[Math.floor(Math.random() * allChars.length)];
+        }
+
+        return password;
+    }
     return(
-        <Text>Generate</Text>
+        <view>
+            <Text>
+                {password}
+            </Text>
+            <Button onClick = {()=>{
+                setPassword(createPassword())
+            }}>
+            Create Password
+            </Button>
+        </view>
     );
 }
+
+
+
+    
